bf8655a8ac63f27aca1b7d77008887ecf4f6e976
yunmo.kang <yunmo.kang@lge.com>
2022-11-01 17:32:23 +0900

    1:29:29
    Change-Id: Ic1f9a2b3c0a061429bf95b576e9e8cb1de635822

--- app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/model/Note.kt
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/model/Note.kt
@@ -16,3 +16,5 @@ data class Note(
         val notecolors = listOf(RedOrange, LightGreen, Violet, BabyBlue, RedPink)
     }
 }
+
+class InvalidNoteException(message:String): Exception(message)
---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/presentation/notes/NotesScreen.kt
@@ -0,0 +1,12 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.presentation.notes
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun (
+    navController : NavController,
+    viewModel : NotesViewModel = hiltViewModel()
+) {
+
+}
